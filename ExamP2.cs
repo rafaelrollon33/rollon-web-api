3402ee736d7d486d6e13e0cdaad9bd96055880bc
rafaelrollon33 <73091503+rafaelrollon33@users.noreply.github.com>
2020-11-24 12:15:01 +0800

    Add files via upload

---
+++ ExamP2.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 24/11/2020
+ * Time: 11:40 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ExamP2
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Label4Click(object sender, EventArgs e)
+		{
+	
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void DataGridView1CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+	 
+	
+		}}}
